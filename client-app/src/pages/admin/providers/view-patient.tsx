7893e503cd0b8947f6bb2e03b3047d1dd8baf11b
Cysteine12 <gbengapeace2001@gmail.com>
2025-09-04 02:13:52 +0100

    type(provider/view-patient): fix errors

--- client-app/src/pages/admin/providers/view-patient.tsx
+++ client-app/src/pages/admin/providers/view-patient.tsx
@@ -23,15 +23,19 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import ProfilePhoto from '../../../components/profile-photo'
 import { usePatient } from '../../../features/patients/hook'
 import type { Patient } from '../../../features/patients/types'
-import { useAppointmentsByPatient } from '../../../features/appointments/hooks'
 import { formatDate } from '../../../lib/utils'
 import { formatPurposeText } from '../../../lib/type'
+import { useAppointmentsByPatient } from '../../../features/appointments/providers/hook'
+import type { Appointment } from '../../../features/appointments/types'
 
 const ViewPatient = () => {
   const { id } = useParams()
   const navigate = useNavigate()
   const { data: patientData, isLoading } = usePatient(id)
-  const { appointments } = useAppointmentsByPatient(id)
+  const { data: appointmentsData } = useAppointmentsByPatient(id, {
+    page: 1,
+    limit: 3,
+  })
 
   const patient: Patient = patientData?.data
 
@@ -248,14 +252,14 @@ const ViewPatient = () => {
             </CardHeader>
             <CardContent>
               <div className="space-y-4">
-                {appointments.length < 1 && (
+                {appointmentsData?.data?.length < 1 && (
                   <div className="text-center text-sm text-muted-foreground">
                     No records yet
                   </div>
                 )}
-                {appointments?.map((appointment, index) => (
+                {appointmentsData?.data?.map((appointment: Appointment) => (
                   <div
-                    key={index}
+                    key={appointment?.id}
                     className="flex items-center justify-between p-3 border rounded-lg"
                   >
                     <div className="flex items-center space-x-3">
